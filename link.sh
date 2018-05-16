bff23331307fb3eb43e3ec17e4357922070bfece
Jorge Luis Beroisa <jberoisa@gmail.com>
2018-05-16 16:24:57 -0300

    added xrdb command

--- link.sh
+++ link.sh
@@ -1,9 +1,13 @@
 
 rm ~/.xinitrc
 ln -T ~/.dotfiles/.xinitrc ~/.xinitrc
+
 rm ~/.config/ranger/rc.conf
 ln -T ~/.dotfiles/.ranger ~/.config/ranger/rc.conf
+
 rm ~/.config/i3/config
 ln -T ~/.dotfiles/.i3 ~/.config/i3/config
+
 rm ~/.Xresources 
 ln -T ~/.dotfiles/.Xresources ~/.Xresources
+xrdb ~/.Xresources 
